49f2695753ebc58512b70a5b9242bbe910baf977
WillEllisContent <WillEllisContent@users.noreply.github.com>
2015-01-20 20:35:56 -0500

    Lab 1
    Lab 1

---
+++ Lab1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Will";
+            char middleInitial = 'M' ;
+            string lastName = "Ellis";
+            string fullName = firstName + " " + middleInitial + ". " + lastName;
+
+            int age = 36;
+            int heightFeet = 5;
+            double heightInches = 8;
+            double totalHeightCM = ((heightFeet * 12) + heightInches) * 2.54;
+
+            bool isCitizen = true;
+            bool canVote = isCitizen && age >= 18;
+
+            Console.WriteLine(fullName);
+            Console.WriteLine(totalHeightCM);
+            Console.WriteLine(canVote);
+        }
+    }
+}
