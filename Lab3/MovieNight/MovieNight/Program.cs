a0036322f012d73df9a837a00d0f732f37c226bd
WillEllisContent <WillEllisContent@users.noreply.github.com>
2015-02-17 18:47:12 -0500

    Lab3

--- Lab3/MovieNight/MovieNight/Program.cs
+++ Lab3/MovieNight/MovieNight/Program.cs
@@ -13,19 +13,41 @@ namespace MovieNight
             Console.Write("Are you going to a Matinee or an Evening show?");
             string MovieType = Console.ReadLine();
 
+            Console.WriteLine();
+
             Console.WriteLine("You selected " + MovieType + ".");
-            Console.ReadLine();
+            Console.WriteLine();
 
             Console.Write("How many full-price adults?");
-            int Adults = int.Parse(Console.ReadLine());
+            double Adults = double.Parse(Console.ReadLine());
 
             Console.Write("How many children?");
-            int Children = int.Parse(Console.ReadLine());
+            double Children = double.Parse(Console.ReadLine());
 
             Console.Write("How many senior citizens?");
-            int Seniors = int.Parse(Console.ReadLine());
+            double Seniors = double.Parse(Console.ReadLine());
+
+            if (MovieType == "Matinee")
+            {
+                double AdultsTickets = Adults * 5.99;
+                double ChildrenTickets = Children * 3.99;
+                double SeniorsTickets = Seniors * 4.50;
+                double TotalTicketPrice = AdultsTickets + ChildrenTickets + SeniorsTickets;
+                Console.WriteLine("Your total ticket price is $" + TotalTicketPrice);
+                Console.ReadLine();
+            }
+            else if (MovieType == "Evening")
+            {
+                double AdultsTickets = Adults * 10.99;
+                double ChildrenTickets = Children * 6.99;
+                double SeniorsTickets = Seniors * 8.50;
+                double TotalTicketPrice = AdultsTickets + ChildrenTickets + SeniorsTickets;
+                Console.WriteLine("Your total ticket price is $" + TotalTicketPrice);
+                Console.ReadLine();
+            }
 
 
-        }
+
+        }   
     }
 }
