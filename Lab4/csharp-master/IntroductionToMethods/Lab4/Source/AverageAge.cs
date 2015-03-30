bb806311a96af810d0b818d499204a1332571e9a
WillEllisContent <WillEllisContent@users.noreply.github.com>
2015-03-30 19:53:38 -0400

    Lab 4
    Lab 4

---
+++ Lab4/csharp-master/IntroductionToMethods/Lab4/Source/AverageAge.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab4
+{
+    public class AverageAge
+    {
+        private double _sumOfAllAges = 0;
+        private int _totalNumberOfPeople = 0;
+
+        public double Get()
+        {
+            return _sumOfAllAges / _totalNumberOfPeople;
+        }
+
+        public void AddPerson(Person NewPerson)
+        {
+         int Age = NewPerson.Age;
+         _sumOfAllAges = _sumOfAllAges + Age;
+         _totalNumberOfPeople = _totalNumberOfPeople + 1;
+        }
+    }
+}
---
+++ Lab4/csharp-master/IntroductionToMethods/Lab4/Source/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lab4
+{
+    public class Person
+    {
+        public int Age;
+        public string FirstName;
+        public string LastName;
+        public Person Spouse;
+        public bool Married;
+
+        public static Person Create(string Person)
+        {
+            Person person = new Person();
+            string firstName;
+            string lastName;
+            int age;
+            bool married;
+            firstName = Question.AskForString("First Name");
+            lastName = Question.AskForString("Last Name");
+            age = Question.AskForInteger("Age");
+            married = Question.AskForBoolean("Married");
+            if (married)
+            {
+                Person Spouse = new Person();
+                string SpouseName = Question.AskForString("Spouse Name");
+                int SpouseAge = Question.AskForInteger("Spouse Age");
+
+            }
+            Console.WriteLine();
+            return person;
+        }
+    }
+}
