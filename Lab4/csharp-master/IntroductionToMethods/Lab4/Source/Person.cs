9e0f6e9ef9afb7d202cb84551a867c63584eaeba
WillEllisContent <WillEllisContent@users.noreply.github.com>
2015-03-30 20:00:36 -0400

    Lab 4
    Lab 4

--- Lab4/csharp-master/IntroductionToMethods/Lab4/Source/Person.cs
+++ Lab4/csharp-master/IntroductionToMethods/Lab4/Source/Person.cs
@@ -13,15 +13,11 @@ namespace Lab4
         public static Person Create(string Person)
         {
             Person person = new Person();
-            string firstName;
-            string lastName;
-            int age;
-            bool married;
-            firstName = Question.AskForString("First Name");
-            lastName = Question.AskForString("Last Name");
-            age = Question.AskForInteger("Age");
-            married = Question.AskForBoolean("Married");
-            if (married)
+            string FirstName = Question.AskForString("First Name");
+            string LastName = Question.AskForString("Last Name");
+            int Age = Question.AskForInteger("Age");
+            bool Married = Question.AskForBoolean("Married");
+            if (Married)
             {
                 Person Spouse = new Person();
                 string SpouseName = Question.AskForString("Spouse Name");
