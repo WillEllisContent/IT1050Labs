88c1e021c27e9d36eb0b3f841d660c8a9cbef80a
WillEllisContent <WillEllisContent@users.noreply.github.com>
2015-04-06 19:36:14 -0400

    Lab 5
    Lab 5

---
+++ Lab5/Lab5/Lab5/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Course
+    {
+        public string CourseName;
+        public int CourseNumber;
+        Student Student;
+    }
+}
---
+++ Lab5/Lab5/Lab5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Course course = new Course();
+            Console.WriteLine("Enter the name of the course: ");
+            string CourseName = Console.ReadLine();
+            Console.WriteLine("Enter the course number: ");
+            int CourseNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("How many students are taking the course");
+            int StudentBase = int.Parse(Console.ReadLine());
+            int Students;
+
+            for (Students = 0; Students < StudentBase; Students++)
+            {
+                Student student = new Student();
+                Console.Write("Student Name: ");
+                student.StudentName = Console.ReadLine();
+            }
+
+            Console.WriteLine(CourseName);
+            Console.WriteLine(CourseNumber);
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
